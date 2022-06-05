18923d112b949e5b3c848858906c3673564d9f83
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-06-05 21:30:12 +0700

    main

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 
 /*
- *  curl_obj.c
+ *  main.c
  *  Copyright (C) Fadhil Riyanto
  *
  *  https://github.com/fadhil-riyanto/ctg.git
@@ -12,15 +12,18 @@
 #include <fcntl.h>
 #include <errno.h>
 #include "ctg.h"
+#include "debug_fn.h"
 
-int main() 
+int main(int argc, char *argv[]) 
 {
-        if (getenv("CTG_BOT_TOKEN") == NULL) {
-                fprintf(stderr, "%s", "error, CTG_BOT_TOKEN not found");
+        DEBUGP("argc total is %d\n", argc);
+        if (argc < 2 && getenv("CTG_BOT_TOKEN") == NULL) {
+                fprintf(stderr, "%s", "error, CTG_BOT_TOKEN not found. or pass the bot token into 2nd argc");
                 return EAGAIN;
-        }
+        } 
+        
         ctg_utils_t *ctgu;
-        ctgu->bot_token = getenv("CTG_BOT_TOKEN");
+        ctgu->bot_token = (getenv("CTG_BOT_TOKEN") == NULL) ? argv[2] : getenv("CTG_BOT_TOKEN");
         
         
 
