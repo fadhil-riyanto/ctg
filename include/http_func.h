44ea91138cf6235a8103c4cf3d6b168e6c43f762
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-06-09 14:25:29 +0700

    fix redefinition

--- include/http_func.h
+++ include/http_func.h
@@ -1,4 +1,5 @@
 #include "stdbool.h"
+#include "key_value.h"
 
 char *http_build_query(key_value_t *data_arr_keyval, size_t len_arr);
 char *urlencode(char *alloc, const char *s, size_t len, bool raw);
