17e4edee9e41a778749600a2947f175a09347889
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-06-10 20:20:55 +0700

    name struct name

--- include/tg_method.h
+++ include/tg_method.h
@@ -1,3 +1,3 @@
 #include "fetchres.h"
 
-tg_json_fetch_res_t* get_updates(ctg_utils_t *maindt, int offset, int limit);
+tg_json_getupdates_t* get_updates(ctg_utils_t *maindt, int offset, int limit);
