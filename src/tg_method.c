6cc300595d7ea763978b9f350fb17fdf019f1e60
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-06-11 15:36:12 +0700

    add condition while curl error, trying until succed

--- src/tg_method.c
+++ src/tg_method.c
@@ -44,11 +44,13 @@ tg_json_getupdates_t* get_updates(ctg_utils_t *maindt, int offset, int limit)
         sprintf(buff, "https://api.telegram.org/bot%s/%s?%s", maindt->bot_token, "getUpdates", urlparam);
         
         DEBUGP("url: %s\n", buff);
+returnreq:
         ks = ch_init();
         curl_req(ks, buff);
 
         if(ks->curlerr == true) {
                 DEBUGP("%s", "curl error while getting data. trying again ... ");
+                goto returnreq;
         }
         free(urlparam);
         free(offset_str);
@@ -58,45 +60,39 @@ tg_json_getupdates_t* get_updates(ctg_utils_t *maindt, int offset, int limit)
         json_object *index_null;
         json_object *is_ok = json_object_object_get(raw, "ok");
         if(json_object_get_boolean(is_ok) != true) {
-                DEBUGP("\n%s", "telegram result returned false");
-        } else {
-                tg_json_getupdates_t *getupdates_res = malloc(sizeof(*getupdates_res));
+                DEBUGE("\n%s", "telegram result returned false");
+                exit(1);
+        } 
+        tg_json_getupdates_t *getupdates_res = malloc(sizeof(*getupdates_res));
 
-                /* get the result */
-                json_object *result = json_object_object_get(raw, "result");
-                index_null = json_object_array_get_idx(result, 0);
+        /* get the result */
+        json_object *result = json_object_object_get(raw, "result");
+        index_null = json_object_array_get_idx(result, 0);
 
-                /* get update_id [root] */
-                json_object *updateid = json_object_object_get(index_null, "update_id");
-                getupdates_res->update_id = json_object_get_int(updateid);
+        /* get update_id [root] */
+        json_object *updateid = json_object_object_get(index_null, "update_id");
+        getupdates_res->update_id = json_object_get_int(updateid);
 
-                /* get [root].message */
-                json_object *messagecl = json_object_object_get(index_null, "message");
+        /* get [root].message */
+        json_object *messagecl = json_object_object_get(index_null, "message");
 
-                /* get [root].message.message_id */
-                json_object *message_id = json_object_object_get(messagecl, "message_id");
-                getupdates_res->message.message_id = json_object_get_int(message_id);
+        /* get [root].message.message_id */
+        json_object *message_id = json_object_object_get(messagecl, "message_id");
+        getupdates_res->message.message_id = json_object_get_int(message_id);
 
-                /* get [root].message.from.id */
-                messagecl = json_object_object_get(index_null, "message");
-                json_object *fromcl = json_object_object_get(messagecl, "from");
-                json_object *from_id = json_object_object_get(fromcl, "id");
-                
-                // DEBUGP("%s\n", json_object_to_json_string_ext(from_id, JSON_C_TO_STRING_PRETTY));
-                DEBUGP("%s\n", json_object_get_string(from_id));
-                DEBUGP("%ld\n", (uint64_t)json_object_get_uint64(from_id));
-                // getupdates_res->message.from.id = json_object_get_int(from_id);
+        /* get [root].message.from.id */
+        messagecl = json_object_object_get(index_null, "message");
+        json_object *fromcl = json_object_object_get(messagecl, "from");
+        json_object *from_id = json_object_object_get(fromcl, "id");
+        
+        // DEBUGP("%s\n", json_object_to_json_string_ext(from_id, JSON_C_TO_STRING_PRETTY));
+        DEBUGP("%s\n", json_object_get_string(from_id));
+        DEBUGP("%ld\n", (uint64_t)json_object_get_uint64(from_id));
+        // getupdates_res->message.from.id = json_object_get_int(from_id);
 
-                free(ks->data);
-                curl_easy_cleanup(ks->ch);
-                free(ks);
-                json_object_put(raw);
-                return getupdates_res;
-                
-        }
         free(ks->data);
+        curl_easy_cleanup(ks->ch);
         free(ks);
-        free(raw);
-        
-        
+        json_object_put(raw);
+        return getupdates_res;
 }
